85829f5329194dd4fb384fccdad032a9c4ae3cd2
Gabriel Amaral <gabriel.r.c.amaral@gmail.com>
2020-08-04 22:07:52 -0300

    add java-16

---
+++ java-16/src/main/java/br/com/codenation/BillingProcessor.java
@@ -0,0 +1,12 @@
+package br.com.codenation;
+
+import br.com.codenation.paymentmethods.PaymentMethod;
+import br.com.codenation.paymentmethods.PriceStrategy;
+
+public class BillingProcessor {
+
+    public Double calculate(Order order) {
+        return order.getPaymentMethod().getPaymentStrategy().calculate(order.getPrice());
+        //throw new RuntimeException("Payment method not implemented");
+    }
+}
